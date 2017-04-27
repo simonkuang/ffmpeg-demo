00aaf713b910ebb42bc3b5e44931f5eb628e9983
simonkuang <me@simonkuang.com>
2017-04-27 14:18:16 +0800

    fix the deprecated warnings

--- src/audio_player/audio_player.cc
+++ src/audio_player/audio_player.cc
@@ -1,7 +1,3 @@
-#ifndef DEMO_AVFILTER_H_
-#define DEMO_AVFILTER_H_
-#pragma once
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -37,13 +33,14 @@ int main(int argc, char* argv[])
   AVFormatContext  *pFormatCtx;
   int        i, audioStream;
   AVCodecContext  *pCodecCtx;
+  AVCodecParameters  *pCodecPar;
   AVCodec      *pCodec;
   AVPacket    *packet;
   uint8_t      *out_buffer;
   AVFrame      *pFrame;
   int ret;
-  uint32_t len = 0;
-  int got_picture;
+  //uint32_t len = 0;
+  //int got_picture;
   int index = 0;
   int64_t in_channel_layout;
   struct SwrContext *au_convert_ctx;
@@ -70,7 +67,7 @@ int main(int argc, char* argv[])
   // Find the first audio stream
   audioStream=-1;
   for(i=0; i < pFormatCtx->nb_streams; i++)
-    if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_AUDIO){
+    if(pFormatCtx->streams[i]->codecpar->codec_type==AVMEDIA_TYPE_AUDIO){
       audioStream=i;
       break;
     }
@@ -81,31 +78,41 @@ int main(int argc, char* argv[])
   }
 
   // Get a pointer to the codec context for the audio stream
-  pCodecCtx=pFormatCtx->streams[audioStream]->codec;
+  pCodecPar = pFormatCtx->streams[audioStream]->codecpar;
 
   // Find the decoder for the audio stream
-  pCodec=avcodec_find_decoder(pCodecCtx->codec_id);
+  pCodec=avcodec_find_decoder(pCodecPar->codec_id);
+  printf("AVCodec Paramters object carrys on the codec id: %d\n", pCodecPar->codec_id);
   if(pCodec==NULL){
     printf("Codec not found.\n");
     return -1;
   }
+  printf("Codec been found has name: %s\n", pCodec->name);
+  printf("Codec has codec id: %d\n", pCodec->id);
+
+  // alloc AVCodecContext
+  pCodecCtx = avcodec_alloc_context3(pCodec);
 
   // Open codec
-  if(avcodec_open2(pCodecCtx, pCodec,NULL)<0){
+  ret = avcodec_open2(pCodecCtx, pCodec, NULL);
+  printf("AVCodec Context initialized return value: %d\n", ret);
+  if (ret < 0) {
     printf("Could not open codec.\n");
     return -1;
   }
 
-  packet=(AVPacket *)av_malloc(sizeof(AVPacket));
+  packet = (AVPacket *)av_malloc(sizeof(AVPacket));
   av_init_packet(packet);
 
   //Out Audio Param
-  uint64_t out_channel_layout=AV_CH_LAYOUT_STEREO;
+  uint64_t out_channel_layout = AV_CH_LAYOUT_STEREO;
+  pCodecCtx->frame_size || (pCodecCtx->frame_size = pCodecPar->frame_size);
   //nb_samples: AAC-1024 MP3-1152
-  int out_nb_samples=pCodecCtx->frame_size;
-  AVSampleFormat out_sample_fmt=AV_SAMPLE_FMT_S16;
-  int out_sample_rate=44100;
-  int out_channels=av_get_channel_layout_nb_channels(out_channel_layout);
+  int out_nb_samples = pCodecCtx->frame_size;
+  printf("Output sample size is: %d\n", pCodecPar->frame_size);
+  AVSampleFormat out_sample_fmt = AV_SAMPLE_FMT_S16;
+  int out_sample_rate = 44100;
+  int out_channels = av_get_channel_layout_nb_channels(out_channel_layout);
   //Out Buffer Size
   int out_buffer_size=av_samples_get_buffer_size(NULL,out_channels ,out_nb_samples,out_sample_fmt, 1);
 
@@ -113,22 +120,53 @@ int main(int argc, char* argv[])
   pFrame=av_frame_alloc();
 
   //FIX:Some Codec's Context Information is missing
-  in_channel_layout=av_get_default_channel_layout(pCodecCtx->channels);
+  pCodecCtx->channels || (pCodecCtx->channels = pCodecPar->channels);
+  pCodecCtx->sample_rate || (pCodecCtx->sample_rate = pCodecPar->sample_rate);
+  printf("channels count from AVCodec Context vars: %d\n", pCodecCtx->channels);
+  printf("sampling rate from AVCodec Context vars: %d\n", pCodecCtx->sample_rate);
+  printf("Channels from AVCodec Parameters is: %d\n", pCodecPar->channels);
+  printf("Channels from AVCodec Parameters is: %d\n", pCodecPar->sample_rate);
+  in_channel_layout = av_get_default_channel_layout(
+    pCodecCtx->channels ? pCodecCtx->channels : pCodecPar->channels);
+  printf("SWResample channel layout count: %lld\n", in_channel_layout);
+  printf("Reached here ~~~\n");
   //Swr
   au_convert_ctx = swr_alloc();
-  au_convert_ctx=swr_alloc_set_opts(au_convert_ctx,out_channel_layout, out_sample_fmt, out_sample_rate,
-                                    in_channel_layout,pCodecCtx->sample_fmt , pCodecCtx->sample_rate,0, NULL);
-  swr_init(au_convert_ctx);
+  if (au_convert_ctx == NULL) {
+    printf("Error in alloc SWResampling Context object.\n");
+    return -1;
+  }
+  au_convert_ctx = swr_alloc_set_opts(au_convert_ctx,out_channel_layout, out_sample_fmt, out_sample_rate,
+                                      //in_channel_layout,pCodecCtx->sample_fmt , pCodecCtx->sample_rate,0, NULL);
+                                      in_channel_layout, (AVSampleFormat)pCodecPar->format , pCodecPar->sample_rate, 0, NULL);
+  printf("Output sample size is: %d\n", pCodecPar->frame_size);
+  if (au_convert_ctx == NULL) {
+    printf("Error in setting SWResampling Context alloc options.\n");
+    return -1;
+  }
+  ret = swr_init(au_convert_ctx);
+  if (ret != 0) {
+    printf("Error in initializing SWResampleContext with ret: %d\n", ret);
+    return -1;
+  }
 
+  printf("packet size is: %d\n", packet->size);
   while(av_read_frame(pFormatCtx, packet)>=0){
     if(packet->stream_index==audioStream){
 
-      ret = avcodec_decode_audio4( pCodecCtx, pFrame,&got_picture, packet);
-      if ( ret < 0 ) {
+      //ret = avcodec_decode_audio4( pCodecCtx, pFrame,&got_picture, packet);
+      ret = avcodec_send_packet(pCodecCtx, packet);
+      if (ret != 0) {
+        printf("Error in decoding process while sending frame.\n");
+        return -1;
+      }
+      ret = avcodec_receive_frame(pCodecCtx, pFrame);
+      if (ret < 0) {
         printf("Error in decoding audio frame.\n");
         return -1;
       }
-      if ( got_picture > 0 ){
+      //if ( got_picture > 0 ){
+      if (ret == 0) {
         swr_convert(au_convert_ctx,&out_buffer, MAX_AUDIO_FRAME_SIZE,(const uint8_t **)pFrame->data , pFrame->nb_samples);
 
         printf("index:%5d\t pts:%lld\t packet size:%d\n",index,packet->pts,packet->size);
@@ -137,7 +175,7 @@ int main(int argc, char* argv[])
         index++;
       }
     }
-    av_free_packet(packet);
+    av_packet_unref(packet);
   }
 
   swr_free(&au_convert_ctx);
@@ -153,5 +191,3 @@ int main(int argc, char* argv[])
   return 0;
 }
 
-
-#endif // DEMO_AVFILTER_H_
---
+++ src/to_aac/to_aac.cc
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define __STDC_CONSTANT_MACROS
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswresample/swresample.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+int main(int argc, char** argv) {
+  return 0
+}
